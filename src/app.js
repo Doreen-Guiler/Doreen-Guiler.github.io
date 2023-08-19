ae83038ac62fcf34caf513aefba8c2a2320c480d
Doreen-Guiler <139405571+Doreen-Guiler@users.noreply.github.com>
2023-08-19 17:22:28 -0400

    Delete src directory

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
-
-function toggleButton() {
-    navList.classList.toggle('show')
-}
-
-hamburgerButton.addEventListener('click', toggleButton)
